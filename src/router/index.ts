5db30bd1e7e8647fc742997dc160c831b8d491f2
jongchan lee <jongchanlee93@gmail.com>
2024-01-24 21:44:47 +0900

    domain

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory, createWebHistory } from "vue-router
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
